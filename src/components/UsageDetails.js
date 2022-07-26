30caadea9714a3e000a99e066a4bf39a4869ce1d
ujjwalkumar93 <pathakujjwal93@gmail.com>
2022-07-26 23:09:44 +0530

    code added

--- src/components/UsageDetails.js
+++ src/components/UsageDetails.js
@@ -12,8 +12,14 @@ import Button from 'react-bootstrap/Button';
 const UsageDetails = () => {
     const [isListView, setIsListView] = useState(false)
     const [ipList , setIpList] = useState([])
-    const [startDate, setStartDate] = useState(new Date());
-
+    const [fromDate, setFromDate] = useState();
+    const [toDate, setToDate] = useState();
+    const [show,setShow] = useState(false)
+    const [selectedIp, setSelectedIp] = useState([])
+    const endDate1 = new Date();
+    endDate1.setDate(endDate1.getDate() + 2);
+    const [endDate, setEndDate] = useState(endDate1);
+    console.log("new Date()+3 is: ", endDate1)
     useEffect(() => {
         fetch('http://localhost:8000/api/server/list',{method:"GET"})
         .then(resp => {return resp.json()})
@@ -29,7 +35,55 @@ const UsageDetails = () => {
         const value = !isListView
         setIsListView(value)
     }
-    //console.log(isListView)
+
+    const handleSelect  = (ip) => {
+        if(selectedIp.includes(ip)){
+            const result = selectedIp.filter(i => i != ip);
+            setSelectedIp([...result])
+        } else {
+            let res = selectedIp
+            res.push(ip)
+            setSelectedIp([...res])
+        }
+    }
+    const handleSubmit = () => {
+        setShow(false)
+        fetch('http://localhost:8000/api/usage/cpu',{
+            method:"POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify({"ipList": selectedIp, "fromDate": fromDate, "toDate": toDate})
+        })
+        .then(resp => {return resp.json()})
+        .then(data => {
+            if(data.message){
+                setIpList(data.message)
+                console.log("running..",data.message)
+            }
+        })
+        console.log("data is: ", fromDate, toDate, selectedIp)
+    }
+
+    const handleDate = (date, target) => {
+        const d = formatDate (date)
+        if(target == "to") {
+            setToDate(d)
+        }
+        if(target == "from") {
+            setFromDate(d)
+        }
+
+        //console.log("formatDate (input) is: ",)
+    }
+    function formatDate (input) {
+        var datePart = input.match(/\d+/g),
+        year = datePart[0], // get only two digits
+        month = datePart[1], day = datePart[2];
+      
+        return day+'/'+month+'/'+year;
+      }
     return(
         <div style={{marginTop:8, padding:20}}>
             <div style={{marginBottom:30}}>
@@ -42,9 +96,9 @@ const UsageDetails = () => {
                     style={{float:'right', marginTop:"8px"}}
                     onChange = {e => {handleView()}}
                 /> 
-                <Dropdown style={{float:'right', marginTop:"8px", marginRight:"20px"}}>
+                <Dropdown style={{float:'right', marginTop:"8px", marginRight:"20px"}} show={show}>
                 <Dropdown.Toggle variant="light" id="filter">
-                    <AiOutlineSetting/>
+                    <AiOutlineSetting onClick={e => setShow(!show)}/>
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
                     {
@@ -54,6 +108,9 @@ const UsageDetails = () => {
                                     id={i.ip}
                                     label ={i.serverName}
                                     style={{marginLeft:18}}
+                                    onClick ={e => {
+                                        handleSelect(i.ip)
+                                    }}
                                 />
                             )
                         })
@@ -65,15 +122,14 @@ const UsageDetails = () => {
                     type="date"
                     id="from"
                     name="from"
-                    onChange={e => {console.log("#######")}}
+                    onChange={e => {handleDate(e.target.value,"from")}}
                     style={{width:"70%",marginLeft:"18px"}}
                 />
                 <input
                     type="date"
-                    label="From date"
                     id="to"
                     name="to"
-                    onChange={e => {console.log("*******")}}
+                    onChange={e => {handleDate(e.target.value,"to")}}
                     style={{width:"70%", marginRight:"12px", marginLeft:"4px"}}
                 />
                 </div>
@@ -81,6 +137,7 @@ const UsageDetails = () => {
                     type= "submit"
                     value="Apply Filter"
                     style={{margin:18, backgroundColor:"#198754", color:"white", borderRadius:5}}
+                    onClick = {e => handleSubmit()}
                 />
                 </Dropdown.Menu>
                 </Dropdown>
