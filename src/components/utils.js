bbd5317c9fda5c08f35a6a751852715b19fb2c74
ujjwalkumar93 <pathakujjwal93@gmail.com>
2022-07-28 02:25:51 +0530

    refactor: code written for date utility

---
+++ src/components/utils.js
@@ -0,0 +1,15 @@
+const formatDate = (dateObj, formate) => {
+    
+    const d = new Date(dateObj); 
+    console.log("local string", d.toLocaleString().split(',')[0] ); 
+    //return d.toLocaleString().split(',')[0]
+
+    if(formate === "yyyy-mm-dd") {
+        return d.toLocaleString().split(',')[0].split("/").reverse().join("-")
+    } else {
+        return d.toLocaleString().split(',')[0]
+    }
+}
+
+export default formatDate;
+
